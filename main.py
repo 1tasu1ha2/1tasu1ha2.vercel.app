0ce7a102260872917528c265f380162e075cad8b
1+1=2 <tororocord@gmail.com>
2025-07-27 17:50:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,15 +8,18 @@ app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.get("/api/random-emoji")
-def random_emoji(length: float=1):
-    if length.is_integer() and length > 0:
-        length = int(length)
-        return JSONResponse(content={
-            "success": True,
-            "result": "".join(random.choices(list(emoji.EMOJI_DATA.keys()), k=length))
-        })
-    else:
+def random_emoji(length: str="1"):
+    try:
+        num = float(length)
+        if num.is_integer() and num>0:
+            return JSONResponse(content={
+                "success": True,
+                "result": "".join(random.choices(list(emoji.EMOJI_DATA.keys()), k=int(num)))
+            })
+        else:
+            raise ValueError
+    except ValueError:
         return JSONResponse(content={
             "success": False,
-            "message": "Please provide a positive integer."
+            "message": "Length must be a positive integer."
         }, status_code=400)
