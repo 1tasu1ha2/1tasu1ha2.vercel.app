39cbabbfd5a62ee138eca61008460d905bba4db8
1+1=2 <tororocord@gmail.com>
2025-07-27 16:14:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from starlette.middleware.cors import CORSMiddleware
 import random, emoji
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.get("/api/random-emoji")
 def random_emoji(length: int=1):
