13388a8c9adcba3bbb33b91c6c3888d02551b414
1+1=2 <tororocord@gmail.com>
2025-07-27 16:01:17 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+import random, emoji
+
+app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
+
+@app.post("/api/random-emoji")
+def random_emoji(length: int=1):
+    res = JSONResponse(content={
+        "success": True,
+        "result": "".join(random.choices(list(emoji.EMOJI_DATA.keys()), k=length))
+    }, status_code=200)
+    return res
