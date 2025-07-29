3556fcbbdcf6d84f1dc814ddcec395a5f01281dc
1+1=2 <tororocord@gmail.com>
2025-07-29 20:46:11 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
-import random, emoji
+import random, emoji, string
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
@@ -15,6 +15,23 @@ def random_emoji(length: str="1"):
             return JSONResponse(content={
                 "success": True,
                 "result": "".join(random.choices(list(emoji.EMOJI_DATA.keys()), k=int(num)))
+            })
+        else:
+            raise ValueError
+    except ValueError:
+        return JSONResponse(content={
+            "success": False,
+            "message": "Length must be a positive integer."
+        }, status_code=400)
+
+@app.get("/api/random-string")
+def random_string(length: str="1"):
+    try:
+        num = float(length)
+        if num.is_integer() and num>0:
+            return JSONResponse(content={
+                "success": True,
+                "result": "".join(random.choices(string.ascii_letters+string.ascii_lowercase+string.ascii_uppercase), k=int(num)))
             })
         else:
             raise ValueError
