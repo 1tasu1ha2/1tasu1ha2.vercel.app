851b94b84f43a3dc30675fcb4fcbc1131f6ff26a
1+1=2 <tororocord@gmail.com>
2025-08-01 15:52:16 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
-import random, emoji, string
+import random, string
+import emoji_data_python
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
+
+emojis = [e.char for e in emoji_data_python.emoji_data if not e.is_skin_tone and e.is_emoji]
 
 @app.get("/api/random-emoji")
 def random_emoji(length: str = "1"):
     try:
         num = float(length)
         if num.is_integer() and num > 0:
-            emojis = [
-                e for e in emoji.EMOJI_DATA.keys()
-                if not any(tone in e for tone in ["🏻", "🏼", "🏽", "🏾", "🏿"])
-            ]
             return JSONResponse(content={
                 "success": True,
                 "result": "".join(random.choices(emojis, k=int(num)))
@@ -29,13 +28,13 @@ def random_emoji(length: str = "1"):
         }, status_code=400)
 
 @app.get("/api/random-string")
-def random_string(length: str="1"):
+def random_string(length: str = "1"):
     try:
         num = float(length)
-        if num.is_integer() and num>0:
+        if num.is_integer() and num > 0:
             return JSONResponse(content={
                 "success": True,
-                "result": "".join(random.choices(string.ascii_letters+string.digits, k=int(num)))
+                "result": "".join(random.choices(string.ascii_letters + string.digits, k=int(num)))
             })
         else:
             raise ValueError
