3defd1c2c1f19960c3f05d0d83420e51ac707c42
1+1=2 <tororocord@gmail.com>
2025-08-01 23:15:15 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
 from starlette.middleware.cors import CORSMiddleware
-import random, emoji, string
+import random, emoji, string, tls_client
 from uuid import uuid4
-from curl_cffi.requests import Session
 
 app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
@@ -64,18 +63,25 @@ async def join(request: Request):
                 "success": False,
                 "message": "Please provide invite."
             }, status_code=400)
-        session = Session()
+        session = tls_client.Session(client_identifier=f"chrome_124", random_tls_extension_order=True)
         session.get("https://discord.com")
         headers = {
-            "authority": "discord.com",
-            "accept": "*/*",
-            "accept-language": "ja",
-            "authorization": token,
-            "content-type": "application/json",
-            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.4 Safari/605.1.15",
-            "x-discord-locale": "ja",
-            "x-debug-options": "bugReporterEnabled",
-            "x-super-properties": "eyJvcyI6ImlPUyIsImJyb3dzZXIiOiJNb2JpbGUgU2FmYXJpIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImphIiwiaGFzX2NsaWVudF9tb2RzIjpmYWxzZSwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKE1hY2ludG9zaDsgSW50ZWwgTWFjIE9TIFggMTBfMTVfNykgQXBwbGVXZWJLaXQvNjA1LjEuMTUgKEtIVE1MLCBsaWtlIEdlY2tvKSBWZXJzaW9uLzE4LjQgU2FmYXJpLzYwNS4xLjE1IiwiYnJvd3Nlcl92ZXJzaW9uIjoiMTguNCIsIm9zX3ZlcnNpb24iOiIxMC4xNS43IiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjQyNTQ4NywiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiY2xpZW50X2xhdW5jaF9pZCI6ImE4ZmJiNzIzLTk3MTktNDM2Ni1iZWYwLTIxODNkZjhiYTliNiIsImxhdW5jaF9zaWduYXR1cmUiOiIyNDNjNjJlMC0wMjZhLTQ0ZjctOTAxOC1hMmFiZDBjYWYxOGYiLCJjbGllbnRfaGVhcnRiZWF0X3Nlc3Npb25faWQiOiI3NmU5YjczYS00ZDc0LTQ0NmMtODFlYS0zMTI3ZjgxZGFiMGUiLCJjbGllbnRfYXBwX3N0YXRlIjoiZm9jdXNlZCJ9"
+            'accept': '*/*',
+            'accept-language': 'en,en-US;q=0.9,en;q=0.8,fr-FR;q=0.7,fr;q=0.6',
+            'authorization': token,
+            'content-type': 'application/json',
+            'origin': 'https://discord.com',
+            'sec-ch-ua': '"Not-A.Brand";v="99", "Chromium";v="124"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'empty',
+            'sec-fetch-mode': 'cors',
+            'sec-fetch-site': 'same-origin',
+            'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.6367.118 Safari/537.36',
+            'x-debug-options': 'bugReporterEnabled',
+            'x-discord-locale': 'en-US',
+            'x-discord-timezone': 'Asia/Saigon',
+            'x-super-properties': 'eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwic3lzdGVtX2xvY2FsZSI6ImVuLVVTIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzEyNC4wLjYzNjcuMTE4IFNhZmFyaS81MzcuMzYiLCJicm93c2VyX3ZlcnNpb24iOiIxMjQuMC42MzY3LjExOCIsIm9zX3ZlcnNpb24iOiIxMCIsInJlZmVycmVyIjoiIiwicmVmZXJyaW5nX2RvbWFpbiI6IiIsInJlZmVycmVyX2N1cnJlbnQiOiIiLCJyZWZlcnJpbmdfZG9tYWluX2N1cnJlbnQiOiIiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfYnVpbGRfbnVtYmVyIjoyOTcyNzQsImNsaWVudF9ldmVudF9zb3VyY2UiOm51bGwsImRlc2lnbl9pZCI6MH0=',        
         }
         data = {
             "session_id": uuid4().hex
