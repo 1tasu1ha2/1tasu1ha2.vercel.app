3fc8c243c801876d25752cbdbf5417c2edb02b5e
1+1=2 <tororocord@gmail.com>
2025-08-01 15:33:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,17 @@ app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
 @app.get("/api/random-emoji")
-def random_emoji(length: str="1"):
+def random_emoji(length: str = "1"):
     try:
         num = float(length)
-        if num.is_integer() and num>0:
+        if num.is_integer() and num > 0:
+            emojis = [
+                e for e in emoji.EMOJI_DATA.keys()
+                if not any(tone in e for tone in ["🏻", "🏼", "🏽", "🏾", "🏿"])
+            ]
             return JSONResponse(content={
                 "success": True,
-                "result": "".join(random.choices(list(emoji.EMOJI_DATA.keys()), k=int(num)))
+                "result": "".join(random.choices(emojis, k=int(num)))
             })
         else:
             raise ValueError
