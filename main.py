d9f9ffc5dcf34020e7fb8415cd2de0634c265b99
1+1=2 <tororocord@gmail.com>
2025-09-07 09:32:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,36 +7,91 @@ app = FastAPI(docs_url=None, redoc_url=None, openapi_url=None)
 
 app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_credentials=True, allow_methods=["*"], allow_headers=["*"])
 
-@app.get("/api/random-emoji")
-def random_emoji(length: str="1"):
+@self.get('/api/random/alphanumeric')
+async def random_alphanumeric(
+    length: int=1,
+    uppercase: bool=True,
+    lowercase: bool=True,
+    digit: bool=True,
+    symbol: bool=False
+):
     try:
-        num = float(length)
-        if num.is_integer() and num > 0:
-            return JSONResponse(content={
-                "success": True,
-                "result": "".join(random.choices(list(emoji.EMOJI_DATA.keys()), k=int(num)))
-            })
-        else:
-            raise ValueError
-    except ValueError:
-        return JSONResponse(content={
-            "success": False,
-            "message": "Length must be a positive integer."
-        }, status_code=400)
+        chars = ''
+        if uppercase:
+            chars += string.ascii_uppercase
+        if lowercase:
+            chars += string.ascii_lowercase
+        if digit:
+            chars += string.digits
+        if symbol:
+            chars += string.punctuation
+        if chars == '':
+            return JSONResponse(
+                content={
+                    'success': False,
+                    'data': {
+                        'message': '\'uppercase\' or \'lowercase\' or \'digit\' or \'symbol\' must be true.'
+                    }
+                },
+                status_code=400
+            )
+        if length < 0:
+            return JSONResponse(
+                content={
+                    'success': False,
+                    'message': '\'length\' must be greater than 0.'
+                },
+                status_code=400
+            )
+        return JSONResponse(
+            content={
+                'success': True,
+                'data': {
+                    'string': ''.join(random.choices(chars, k=length))
+                }
+            },
+            status_code=200
+        )
+    except Exception as err:
+        return JSONResponse(
+            content={
+                'success': False,
+                'data': {
+                    'message': str(err)
+                }
+            },
+            status_code=500
+        )
 
-@app.get("/api/random-string")
-def random_string(length: str="1"):
+@self.get('/api/random/emoji')
+async def random_emoji(
+    length: int=1
+):
     try:
-        num = float(length)
-        if num.is_integer() and num > 0:
-            return JSONResponse(content={
-                "success": True,
-                "result": "".join(random.choices(string.ascii_letters + string.digits, k=int(num)))
-            })
-        else:
-            raise ValueError
-    except ValueError:
-        return JSONResponse(content={
-            "success": False,
-            "message": "Length must be a positive integer."
-        }, status_code=400)
+        if length < 0:
+            return JSONResponse(
+                content={
+                    'success': False,
+                    'message': '\'length\' must be greater than 0.'
+                },
+                status_code=400
+            )
+        return JSONResponse(
+            content={
+                'success': True,
+                'data': {
+                    'string': ''.join(random.choices(list(emoji.EMOJI_DATA.keys()), k=length))
+                }
+            },
+            status_code=200
+        )
+    except Exception as err:
+       return JSONResponse(
+           content={
+                'success': False,
+                'data': {
+                    'message': str(err)
+                }
+            },
+            status_code=500
+        )
